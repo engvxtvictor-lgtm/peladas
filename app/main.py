461aa10554617c99eee700ab30dcb44d3729dd4e
Jose Victor de Moura Rufino <engvxtvictor@gmail.com>
2026-05-06 20:22:34 -0300

    feat: estrutura base do projeto + FastAPI rodando

---
+++ app/main.py
@@ -0,0 +1,13 @@
+﻿from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from app.database import engine, Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title='Sistema de Peladas')
+
+templates = Jinja2Templates(directory='app/templates')
+
+@app.get('/')
+def index():
+    return {'status': 'ok', 'message': 'Sistema de Peladas rodando!'}
