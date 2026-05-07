7150662735745209027233815f4dbef618366375
Jose Victor de Moura Rufino <engvxtvictor@gmail.com>
2026-05-06 22:16:46 -0300

    feat: frontend base com Jinja2 e HTMX

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-﻿from fastapi import FastAPI
+﻿from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from app.database import engine, Base
 import app.models
 from app.routes import jogadores, partidas
@@ -13,6 +14,6 @@ templates = Jinja2Templates(directory='app/templates')
 app.include_router(jogadores.router)
 app.include_router(partidas.router)
 
-@app.get('/')
-def index():
-    return {'status': 'ok', 'message': 'Sistema de Peladas rodando!'}
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(request, 'index.html')
